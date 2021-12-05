02bf5060ee3213801b44a194f9444c45448fc340
EMERIC237 <bestvemeric@gmail.com>
2021-12-05 17:43:29 -0500

    messages order updated

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -5,11 +5,14 @@ import moment from "moment";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
+  messages.sort((messageA, messageB) => {
+    return moment(messageA.createdAt).diff(messageB.createdAt);
+  });
+  console.log(messages);
   return (
     <Box>
       {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
-
         return message.senderId === userId ? (
           <SenderBubble key={message.id} text={message.text} time={time} />
         ) : (
